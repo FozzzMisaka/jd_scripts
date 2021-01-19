2f4c15a57c843972f0c0cd8f89eebd2dbd59dc6d
FozzzMisaka <37243380+FozzzMisaka@users.noreply.github.com>
2021-01-20 02:35:20 +0800

    Update jd_joy_help.js

--- jd_joy_help.js
+++ jd_joy_help.js
@@ -34,8 +34,6 @@ let ACT_ID = 'A_112790_R_1_D_20201028'
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 let inviteCodes = [
-  '',
-  '',
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
